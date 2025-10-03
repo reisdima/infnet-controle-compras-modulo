b5aa6b6adb03834cb380a38f9936450efb8e6a10
Caio Vincenzo Reis Dima <cvincenzo@ciandt.com>
2025-10-02 21:49:50 -0300

    Implementação de autorização para api de produtos, usando anotação em métodos da controller

--- main-app/src/main/java/br/edu/infnet/gestao_compras/controller/ProdutoController.java
+++ main-app/src/main/java/br/edu/infnet/gestao_compras/controller/ProdutoController.java
@@ -2,11 +2,11 @@ package br.edu.infnet.gestao_compras.controller;
 
 import br.edu.infnet.gestao_compras.dto.request.ProdutoRequestDTO;
 import br.edu.infnet.gestao_compras.dto.response.ProdutoResponseDTO;
-import br.edu.infnet.gestao_compras.model.domain.Produto;
 import br.edu.infnet.gestao_compras.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +21,7 @@ public class ProdutoController {
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<List<ProdutoResponseDTO>> obterLista() {
         List<ProdutoResponseDTO> lista = produtoService.obterLista();
         if (lista.isEmpty()) {
@@ -30,17 +31,16 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<ProdutoResponseDTO> obterPorId(@PathVariable("id") Integer id) {
-        ProdutoResponseDTO produto = new ProdutoResponseDTO(produtoService.obterPorId(id));
-        if (produto == null) {
-            return ResponseEntity.noContent().build();
-        }
+        var produto = new ProdutoResponseDTO(produtoService.obterPorId(id));
         return ResponseEntity.ok(produto);
     }
 
     @GetMapping("/codigo/{codigoDeBarras}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public ResponseEntity<ProdutoResponseDTO> obterPorId(@PathVariable("id") String codigoDeBarras) {
-        Produto produto = this.produtoService.obterProdutoPorCodigoDeBarras(codigoDeBarras);
+        var produto = this.produtoService.obterProdutoPorCodigoDeBarras(codigoDeBarras);
         if (produto == null) {
             return ResponseEntity.noContent().build();
         }
@@ -48,12 +48,14 @@ public class ProdutoController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<Void> excluir(@PathVariable("id") Integer id) {
         produtoService.excluir(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<ProdutoResponseDTO> incluir(@Valid @RequestBody ProdutoRequestDTO produto) {
         ProdutoResponseDTO novoProduto = produtoService.incluir(produto);
 
@@ -61,6 +63,7 @@ public class ProdutoController {
     }
 
     @PutMapping(value = "/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<ProdutoResponseDTO> alterar(@PathVariable("id") Integer id, @RequestBody ProdutoRequestDTO produto) {
         ProdutoResponseDTO produtoAlterado = produtoService.alterar(id, produto);
 
